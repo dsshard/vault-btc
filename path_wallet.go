4d340ab8f12c71203ce99cb7b72e00b23fc08793
Steven Green <dsshard@gmail.com>
2025-04-17 21:25:49 +0300

    Add go mod

--- path_wallet.go
+++ path_wallet.go
@@ -22,7 +22,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
